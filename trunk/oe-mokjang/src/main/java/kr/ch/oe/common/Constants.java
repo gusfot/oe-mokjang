da75985f943e0eb5b03ed5812fd762c8d7a1d421
gusfot <gusfot@ac132302-421a-f201-6c55-26c011ee5e26>
2013-08-31 11:37:50 +0000

    공통 상수 정

---
+++ trunk/oe-mokjang/src/main/java/kr/ch/oe/common/Constants.java
@@ -0,0 +1,8 @@
+package kr.ch.oe.common;
+
+public class Constants {
+	public static final int GROUP_CHURCH = 1;
+	public static final int GROUP_GYOGU = 2;
+	public static final int GROUP_ZO = 3;
+	public static final int GROUP_MOKJANG = 4;
+}
