f17a05cbf3627db48720bada25004bcba1d1f12f
gusfot <gusfot@ac132302-421a-f201-6c55-26c011ee5e26>
2013-08-31 11:38:14 +0000

    Group type 추

--- oe-mokjang/src/main/java/kr/ch/oe/model/Mokjang.java
+++ oe-mokjang/src/main/java/kr/ch/oe/model/Mokjang.java
@@ -3,11 +3,33 @@
  */
 package kr.ch.oe.model;
 
+import java.util.List;
+
+import kr.ch.oe.common.Constants;
+
 /**
  * @author gusfot
  *
  */
 public class Mokjang extends AbstractGroup {
+
+	
+	public Mokjang() {
+		super();
+		setType(Constants.GROUP_MOKJANG);
+	}
+
+	@Override
+	public List<Person> getMembers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMembers(List<Person> members) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
